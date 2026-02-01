651488e9654063fbff45f4fc77e7f60e52379854
BeB55 <brianebaptista@gmail.com>
2026-02-01 17:51:59 -0300

    preparacion para render 3

--- fayticell/settings.py
+++ fayticell/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +77,13 @@ WSGI_APPLICATION = 'fayticell.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
+
 
 
 SOCIALACCOUNT_PROVIDERS = {
