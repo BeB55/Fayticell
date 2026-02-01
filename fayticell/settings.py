3d832749429c81460ec05a2bbc35b7278b22ff65
BeB55 <brianebaptista@gmail.com>
2026-02-01 19:58:55 -0300

    preparacion para render 8

--- fayticell/settings.py
+++ fayticell/settings.py
@@ -148,6 +148,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
