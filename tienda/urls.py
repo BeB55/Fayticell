12812d54b6f94a3fbba9ffaf45fe9bbda9ad9026
BeB55 <brianebaptista@gmail.com>
2026-02-03 15:07:31 -0300

    cambio tmeplate de checkout y arreglo noReverseMatch

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'tienda'
+
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('producto/<int:producto_id>/', views.detalle_producto, name='detalle_producto'),
