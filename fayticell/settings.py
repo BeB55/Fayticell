d9bff6d7991829d5eb50f029b305f610972db6cb
BeB55 <brianebaptista@gmail.com>
2026-02-09 18:30:54 -0300

    Los que no se registran, no compran

--- fayticell/settings.py
+++ fayticell/settings.py
@@ -79,14 +79,14 @@ WSGI_APPLICATION = 'fayticell.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
+
 
 
 
